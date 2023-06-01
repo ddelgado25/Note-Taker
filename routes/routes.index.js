5abf4da0a242155abd886116192b3ded871c886c
ddelgado25 <118149533+ddelgado25@users.noreply.github.com>
2023-05-31 20:40:08 -0400

    route fix

--- routes/routes.index.js
+++ routes/routes.index.js
@@ -4,12 +4,12 @@ const indexRoute = require("express").Router();
 indexRoute.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/index.html"));
     
-})
+});
 
 indexRoute.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/notes.html"));
 
-});
+})
 
 
 module.exports = indexRoute;
