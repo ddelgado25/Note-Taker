5c7ca0f2997aeb13abfca1d18d9dd50473037642
ddelgado25 <118149533+ddelgado25@users.noreply.github.com>
2023-05-31 20:41:42 -0400

    route issue

--- routes/routes.index.js
+++ routes/routes.index.js
@@ -1,14 +1,14 @@
 const path = require("path");
 const indexRoute = require("express").Router();
 
-indexRoute.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
-    
-});
-
 indexRoute.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/notes.html"));
 
+});
+
+indexRoute.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+    
 })
 
 
